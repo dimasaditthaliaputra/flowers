81fb6064fb4fd375f8c38de371ebf6cca1d4c01b
dimasaditt <98073807+dimasaditthaliaputra@users.noreply.github.com>
2024-06-08 01:29:15 +0700

    Update script letter.js

--- script.js
+++ script.js
@@ -7,3 +7,39 @@ onload = () => {
 
 var audio = document.getElementById("background-music");
 audio.volume = 0.5;
+
+document.addEventListener("DOMContentLoaded", function () {
+  const envelopeIcon = document.querySelector(".clickable-envelope i");
+  const backgroundOverlay = document.querySelector(".background-overlay");
+  const container = document.querySelector(".container");
+  const closeButton = document.querySelector(".close-button");
+
+  envelopeIcon.addEventListener("click", function (event) {
+    backgroundOverlay.style.opacity = "1";
+    backgroundOverlay.style.pointerEvents = "auto";
+    container.style.display = "grid";
+  });
+
+  backgroundOverlay.addEventListener("click", function () {
+    backgroundOverlay.style.opacity = "0";
+    backgroundOverlay.style.pointerEvents = "none";
+    container.style.display = "none";
+  });
+
+  backgroundOverlay.addEventListener("click", function () {
+    backgroundOverlay.style.opacity = "0";
+    backgroundOverlay.style.pointerEvents = "none";
+    container.style.display = "none";
+  });
+
+  closeButton.addEventListener("click", function () {
+    backgroundOverlay.style.opacity = "0";
+    backgroundOverlay.style.pointerEvents = "none";
+    container.style.display = "none";
+  });
+});
+
+const envelope = document.querySelector(".envelope-wrapper");
+envelope.addEventListener("click", () => {
+  envelope.classList.toggle("flap");
+});
